0d52a7cc1e6176a2ebec487ef5c6a3ce952484e2
n0izestr3am <n0izestr3am@gmail.com>
2023-07-09 02:32:29 +0700

    fi route callback

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-Route::prefix('oauth/sso')->namespace('n0izestr3am\\SSO\\Http\\Controllers')->middleware('web')->group(function()
+Route::prefix('sso')->namespace('n0izestr3am\\SSO\\Http\\Controllers')->middleware('web')->group(function()
 {
-	Route::get('/sso/login', 'SSOController@getLogin')->name('sso.login');
-	Route::get('/sso/callback', 'SSOController@getCallback')->name('sso.callback');
-	Route::get('/sso/connect', 'SSOController@connectUser')->name('sso.connect');
+	Route::get('/login', 'SSOController@getLogin')->name('sso.login');
+	Route::get('/callback', 'SSOController@getCallback')->name('sso.callback');
+	Route::get('/connect', 'SSOController@connectUser')->name('sso.connect');
 });
